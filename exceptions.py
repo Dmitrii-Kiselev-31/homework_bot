e785bcee8762a4b1a79d8f9054a5e2e28ad16421
Dmitrii-Kiselev-31 <106881589+Dmitrii-Kiselev-31@users.noreply.github.com>
2022-08-30 13:56:04 +0300

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,17 +1,1 @@
-import os
 
-
-class IsNot200Error(Exception):
-    """Ответ соединения сервера, не равен 200."""
-
-
-class EmptyListError(Exception):
-    """Список или словарь пуст."""
-
-
-class UndocumentedStatusError(Exception):
-    """Недокументированный статус домашней работы."""
-
-
-class RequestError(Exception):
-    """Ошибка запроса."""
